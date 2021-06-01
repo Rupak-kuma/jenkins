f6394d2ae896097c828c350a33f18d0a4c53f354
Rupak-kuma <84894190+Rupak-kuma@users.noreply.github.com>
2021-06-01 11:49:43 +0530

    Update Application.java

--- src/main/java/com/hcl/Application.java
+++ src/main/java/com/hcl/Application.java
@@ -4,19 +4,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
 
 import com.hcl.beans.Employee;
 
 @SpringBootApplication
 @Configuration
-public class Application {
+public class Application extends SpringBootServletInitializer
+ {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
-		//Employee e = context.getBean(Employee.class);
-		//e.setId(1);
-		//e.setName("rupak");
-		//System.out.println(e);
+		 @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+         return application.sources(SpringbootFoodApplication.class);
+    }
+
 	}
 
 }
